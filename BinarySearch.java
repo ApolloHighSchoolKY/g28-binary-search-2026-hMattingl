0b14f47db709a75df0a506f553237aa638fe3190
hMattingl <hayden.mattingly2@stu.daviess.kyschools.us>
2026-04-02 02:34:31 +0000

    Commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,31 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		int top = ray.length-1;
+        int bottom = 0;
+        int guess = (top+bottom)/2;
+        boolean found = false;
+
+		Arrays.sort(ray);
+
+		while ((!found) && (top != bottom))
+		{
+			if(ray[guess] == item)
+				found = true;
+			else if(ray[guess] < item)
+				bottom = guess;
+			else if(ray[guess] > item)
+				top = guess;
+
+			guess = (top+bottom)/2;
+		}
+
+		if(!found)
+			guess = -1;
+
+		return guess;
+
+
 
 	}
 }
